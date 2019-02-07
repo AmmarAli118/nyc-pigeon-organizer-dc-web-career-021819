a01aad7e807fccb4ec303a2591ab493e3defb1a5
Ammar Ali <funnyguy118@gmail.com>
2019-02-07 18:51:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  new_hash = {}
+  
+  data.each do |attribute, attribute_hash|
+    
+    attribute_hash.each do |value, name_array|
+        
+      name_array.each_with_index do |name, index|
+      
+         if new_hash[name].nil?
+          new_hash[name] = {}
+        end
+        
+        new_hash[name][attribute] ||= []
+        new_hash[name][attribute] << value.to_s
+
+        
+        #if new_hash[name][type].nil?
+        #  new_hash[name][type] = type_value
+        #end
+      
+      end 
+      
+    end
+  
+  end
+  #binding.pry
+  new_hash
+  
 end
