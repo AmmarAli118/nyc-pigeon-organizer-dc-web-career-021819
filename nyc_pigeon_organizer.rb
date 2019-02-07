2a8c9a00173f232e4148fb11c32a96b5cfa377cf
Ammar Ali <funnyguy118@gmail.com>
2019-02-07 18:57:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,13 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  
   data.each do |attribute, attribute_hash|
     attribute_hash.each do |value, name_array|
       name_array.each do |name|
-      
-        #if new_hash[name].nil?
-        #end
-        
         new_hash[name] ||= {}
         new_hash[name][attribute] ||= []
         new_hash[name][attribute] << value.to_s
-
       end
     end
   end
-  
   new_hash
 end
